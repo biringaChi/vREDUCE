c58689077e398cdd8a786509304606cb3825e039
Biringa Chidera <biringachidera@gmail.com>
2023-09-20 15:19:49 -0400

    generalized repr

--- vreduce/representation/repr.py
+++ vreduce/representation/repr.py
@@ -1,18 +1,17 @@
 __author__ = "biringaChi (Chidera Biringa)" 
 
-def prepare_sequence(sequence):
-    d2a_stmts, d2a_exprs, d2a_decls = sequence
-    d2a_stmts_sequence, d2a_exprs_sequence, d2a_decls_sequence = [], [], []
-    for ftr_stmts, ftr_exprs, ftr_decls in zip(d2a_stmts, d2a_exprs, d2a_decls, ):
-        d2a_stmts_sequence.append("".join([str(data) for data in ftr_stmts]))
-        d2a_exprs_sequence.append("".join([str(data) for data in ftr_exprs]))
-        d2a_decls_sequence.append("".join([str(data) for data in ftr_decls]))
-    train_sequence = []
-    for stmts_seq, exprs_seq, decls_seq in zip(d2a_stmts_sequence, d2a_exprs_sequence, d2a_decls_sequence):
+def prepare_sequence(features):
+    stmts, exprs, decls = features
+    stmts_sequence, exprs_sequence, decls_sequence = [], [], []
+    for ftr_stmts, ftr_exprs, ftr_decls in zip(stmts, exprs, decls):
+        stmts_sequence.append("".join([str(data) for data in ftr_stmts]))
+        exprs_sequence.append("".join([str(data) for data in ftr_exprs]))
+        decls_sequence.append("".join([str(data) for data in ftr_decls]))
+    sequence = []
+    for stmts_seq, exprs_seq, decls_seq in zip(stmts_sequence, exprs_sequence, decls_sequence):
         temp = stmts_seq + exprs_seq + decls_seq
-        train_sequence.append(temp)
-    return train_sequence
-
+        sequence.append(temp)
+    return sequence
 
 def batch(sequence, nsteps = 1):
     for idx in range(0, len(sequence), nsteps):
